1a207f00c057b9d381443bb2f0a6b1fc968cbeb3
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-10-14 12:24:15 +0900

    chore: 인증 서비스 프로젝트 세팅

---
+++ src/main/kotlin/kr/co/fit_a_pet_auth/FitAPetAuthApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.fit_a_pet_auth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FitAPetAuthApplication
+
+fun main(args: Array<String>) {
+	runApplication<FitAPetAuthApplication>(*args)
+}
---
+++ src/test/kotlin/kr/co/fit_a_pet_auth/FitAPetAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.fit_a_pet_auth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FitAPetAuthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
